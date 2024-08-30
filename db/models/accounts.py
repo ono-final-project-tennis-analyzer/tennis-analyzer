c769e37fba1188c951e925764b730d7524b41d04
rotembabani <rotembabmen@gmail.com>
2024-08-30 11:22:12 +0300

    remastered account_store and model

--- db/models/accounts.py
+++ db/models/accounts.py
@@ -1,87 +1,17 @@
-from datetime import datetime
-
-from bson import ObjectId
 from .base_model import BaseModel
-from . import db
-from pydantic import BaseModel as PydanticBaseModel
-from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy import Column, Integer, String, DateTime
 
 
 class Account(BaseModel):
-    def __init__(self, username: str, email: str, password: str):
-        self.id = str(ObjectId())
-        self.username = username
-        self.email = email
-        self.password = generate_password_hash(password)
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
-
-    def save(self):
-        super().save()
-
-        account_data = AccountModel(
-            id=self.id,
-            username=self.username,
-            email=self.email,
-            password=self.password,
-            created_at=self.created_at,
-            updated_at=self.updated_at
-        )
-
-        db.accounts.insert_one(account_data.dict())
-
-    @staticmethod
-    def get_all():
-        data = db.accounts.find()
-        return [Account.from_dict(item) for item in data]
-
-    @staticmethod
-    def get_by_id(account_id):
-        data = db.accounts.find_one({"_id": ObjectId(account_id)})
-        if data:
-            return Account.from_dict(data)
-
-    @staticmethod
-    def update(account_id, data):
-        db.accounts.update_one({"_id": account_id}, {"$set": data})
-
-    @staticmethod
-    def delete(account_id):
-        db.accounts.delete_one({"_id": account_id})
-
-    @staticmethod
-    def get_by_username(username):
-        data = db.accounts.find_one({"username": username})
-        if data:
-            return Account.from_dict(data)
-
-    @staticmethod
-    def get_by_email(email):
-        data = db.accounts.find_one({"email": email})
-        if data:
-            return Account.from_dict(data)
-
-    @staticmethod
-    def login(email, password):
-        account = Account.get_by_email(email)
-        if account and check_password_hash(account.password, password):
-            return account
-
-    @staticmethod
-    def from_dict(data):
-        account = Account(data['username'], data['email'], data['password'])
-        account.id = str(data['_id'])
-        account.created_at = data['created_at']
-        account.updated_at = data['updated_at']
-        account.password = data['password']
-        return account
-
-    def get_id(self):
-        return self.id
+    __table__ = 'accounts'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
 
     @property
     def is_active(self):
-        return True  # Customize this if you need an actual field for this
+        return True
 
     @property
     def is_authenticated(self):
--- db/stores/account_store.py
+++ db/stores/account_store.py
@@ -1,47 +1,66 @@
 from bson.objectid import ObjectId
-from models.accounts import Account
+from sqlalchemy.orm import Session
+from ..models.accounts import Account
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class AccountStore:
-    def create_account(self, username, email, password):
-            account = Account(username=username, email=email, password=password)
-            account.save()
-            return account
 
+    def __init__(self, session: Session):
+        self._session = session
+
+    def create_account(self, username, email, password):
+        hash_password = generate_password_hash(password)
+        account = Account(username=username, email=email, password=hash_password)
+        self._session.add(account)
+        self._session.commit()
+        return self.get_account_for_return(account)
 
     def get_account(self, account_id):
-        account = Account.get_by_id(ObjectId(account_id))
+        account = self._session.query(Account).filter(Account.id == account_id).first()
         if account:
-            return serialize_account(account)
+            return self.get_account_for_return(account)
+        return None
 
     def update_account(self, account_id, data):
-        Account.update(ObjectId(account_id), data)
+        self._session.query(Account).filter(Account.id == account_id).update(data)
+        self._session.commit()
+        return True
 
     def delete_account(self, account_id):
-        Account.delete(ObjectId(account_id))
+        self._session.query(Account).filter(Account.id == account_id).delete()
+        self._session.commit()
+        return True
 
     def list_accounts(self):
-        accounts = Account.get_all()
-        serialized_accounts = [serialize_account(account) for account in accounts]
-        account_count = len(serialized_accounts)
+        query = self._session.query(Account)
+        accounts = query.all()
+        count = query.count()
         return {
-            "accounts": serialized_accounts,
-            "count": account_count
+            "accounts": accounts,
+            "count": count,
         }
 
     def get_account_by_email(self, email):
-        account = Account.get_by_email(email)
+        account = self._session.query(Account).filter(Account.email == email).first()
         if account:
-            return serialize_account(account)
-
+            return self.get_account_for_return(account)
+        else:
+            return None
 
     def login(self, email, password):
-        return Account.login(email, password)
+        account = self._session.query(Account).filter(Account.email == email).first()
+        if account and check_password_hash(account.password, password):
+            return {account.id, account.username, account.email, account.created_at, account.updated_at}
+        else:
+            return None
 
-def serialize_account(account):
-    return {
-        "id": account.id,
-        "username": account.username,
-        "email": account.email,
-        "created_at": account.created_at,
-        "updated_at": account.updated_at
-    }
+    @staticmethod
+    def get_account_for_return(account):
+        return {
+            "id": account.id,
+            "username": account.username,
+            "email": account.email,
+            "created_at": account.created_at,
+            "updated_at": account.updated_at
+        }
