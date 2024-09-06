80eaad402d2976a8a414df2e024e2b9399cdfafc
rotembabani <rotembabmen@gmail.com>
2024-09-06 14:27:26 +0300

    added videos component with upload file components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import "@mantine/core/styles.css";
-import {Button, MantineProvider, Modal} from "@mantine/core";
+import {MantineProvider, Modal} from "@mantine/core";
 import {BrowserRouter, Outlet, Route, Routes} from "react-router-dom";
 import Home from "./components/Pages/home.tsx";
 import Login from "./components/Pages/login.tsx";
 import {PrivateRoute} from "./components/utils/PrivateRoute.tsx";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import Layout from "./components/Layout/layout.component.tsx";
+import {Videos} from "./components/Pages/videos.tsx";
 
 function App() {
     const queryClient = new QueryClient()
@@ -17,7 +18,7 @@ function App() {
                         <Route path="/login" element={<Login/>}/>
                         <Route path="/" element={<PrivateRoute><Layout/></PrivateRoute>}>
                             <Route index element={<Home/>}/>
-                            <Route path="videos" element={<div>videos <Outlet/></div>}>
+                            <Route path="videos" element={<Videos/>}>
                                 <Route path="videoUpload"
                                        element={<Modal onClose={() => {
                                        }} opened={true}/>}/>
---
+++ frontend/src/components/Pages/videos.tsx
@@ -0,0 +1,43 @@
+import {Button, FileButton, Group, Text} from "@mantine/core";
+
+import {Outlet} from "react-router-dom";
+import {useRef, useState} from "react";
+
+export const Videos = () => {
+    const [file, setFile] = useState<File | null>(null);
+    const resetRef = useRef<() => void>(null);
+    const uploadVideo = () => {
+        
+    }
+    const clearFile = () => {
+        setFile(null);
+        resetRef.current?.();
+    };
+    return (
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyItems: "center",
+
+        }}>
+            <Group justify="center">
+                <FileButton resetRef={resetRef} onChange={setFile} accept="video/avi,video/mp4">
+                    {(props) => <Button {...props}>Upload Video</Button>}
+                </FileButton>
+                <Button disabled={!file} color="red" onClick={clearFile}>
+                    Reset
+                </Button>
+                <Button color={"green"} disabled={!file} onClick={uploadVideo}>
+                    Upload
+                </Button>
+            </Group>
+            {file && (
+                <Text size="sm" ta="center" mt="sm">
+                    Picked file: {file.name}
+                </Text>
+            )}
+
+            <Outlet/>
+        </div>
+    )
+}
