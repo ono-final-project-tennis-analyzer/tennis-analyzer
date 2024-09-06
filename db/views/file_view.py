3668730025a0d13612a026725385b18dadb87191
rotembabani <rotembabmen@gmail.com>
2024-09-06 15:51:24 +0300

    added file upload logic but doesnt work

--- db/views/file_view.py
+++ db/views/file_view.py
@@ -1,6 +1,7 @@
+import os
 import uuid
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 from celery_app.task_queue import TaskQueue, TaskItem
 from db.models import create_session
@@ -9,35 +10,52 @@ from storage_client import StorageClient
 
 file_bp = Blueprint('file', __name__)
 
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+UPLOAD_FOLDER = os.path.join(PROJECT_ROOT, 'uploads')
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
 
 @file_bp.route('/', methods=['POST'])
 def create_file():
-    storage = StorageClient()
-    task_queue = TaskQueue()
+    try:
+        if 'file' not in request.files:
+            return jsonify({"error": "No file provided"}), 400
 
-    file_buffer = "..."
-    file_name = f"{uuid.uuid4()}.mp4"
-    account_id = 12
+        file = request.files['file']
 
-    with open(file_name, "w") as temp:
-        temp.write(file_buffer)
+        storage = StorageClient()
+        task_queue = TaskQueue()
 
-    storage.upload_file(account_id=str(account_id), file_name=file_name, file_path=file_buffer)
+        file_buffer = file.read()
+        file_name = f"{uuid.uuid4()}.mp4"
+        account_id = 12
 
-    meta = {
-        "file_name": file_name,
-        "account_id": str(account_id),
-    }
+        temp_file_path = os.path.join(UPLOAD_FOLDER, file_name)
 
-    with create_session() as session:
-        store = EventStore(session)
-        event = store.create_event(name="video_upload", account_id=str(account_id), meta=meta)
+        with open(file_name, "w") as temp:
+            temp.write(file_buffer)
 
-        task_queue.add_task(TaskItem(
-            event_id=event.id,
-            name="video_task",
-            type="Analyze",
-            meta=meta
-        ))
+        storage.upload_file(account_id=str(account_id), file_name=file_name, file_path=file_buffer)
 
-        return jsonify({"status": "True", "event_id": event.id}), 200
+        meta = {
+            "file_name": file_name,
+            "account_id": str(account_id),
+        }
+
+        with create_session() as session:
+            store = EventStore(session)
+            event = store.create_event(name="video_upload", account_id=str(account_id), meta=meta)
+
+            task_queue.add_task(TaskItem(
+                event_id=event.id,
+                name="video_task",
+                type="Analyze",
+                meta=meta
+            ))
+            os.remove(temp_file_path)
+            return jsonify({"status": "True", "event_id": event.id}), 200
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return jsonify({"error": str(e)}), 500
