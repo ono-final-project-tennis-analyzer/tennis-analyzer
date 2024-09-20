1c8987cc0e1669ec5c871fcab090b71f660906ab
rotembabani <rotembabmen@gmail.com>
2024-09-20 12:22:16 +0300

    added Grid col system skeleton

--- frontend/src/components/Pages/home.tsx
+++ frontend/src/components/Pages/home.tsx
@@ -1,17 +1,26 @@
-import {Card, Grid} from "@mantine/core";
+import {Card, Grid, Title} from "@mantine/core";
 import {DropzoneComponent} from "../Dashboard/Dropzone/DropzoneComponent.tsx";
 
 export default function Home() {
 
 
     return (
-        <Grid gutter={8}>
-            <Grid.Col span={3}>
-                <Card shadow="sm" padding="lg" radius="md" withBorder>
-                    <DropzoneComponent/>
-                </Card>
-            </Grid.Col>
 
+        <Grid gutter={"md"}>
+            <Grid.Col span={12}>
+                <Title>Welcome!</Title>
+            </Grid.Col>
+            <Grid.Col dir={"vertical"}>
+                <Grid.Col span={6}>
+                    <Card shadow="sm" padding="lg" radius="md" withBorder>
+                        <DropzoneComponent/>
+                    </Card>
+                </Grid.Col>
+
+                <Grid.Col span={6}>
+
+                </Grid.Col>
+            </Grid.Col>
         </Grid>
     );
 }
